27ae1868d2c21a697ef06934f065f698b52d532f
mariam <mariammuza@mail.ru>
2023-04-27 12:54:45 +0500

    fifth commit (added sum function =3 )

--- app/src/main/java/com/mariambu/flowernotebook/activities/EditSalesActivity.java
+++ app/src/main/java/com/mariambu/flowernotebook/activities/EditSalesActivity.java
@@ -7,18 +7,23 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
 
 import com.mariambu.flowernotebook.DB.DatabaseHelper;
 import com.mariambu.flowernotebook.R;
 
 public class EditSalesActivity extends AppCompatActivity {
-    EditText name_input, price_input, count_input, sum_input;
-    Button save_btn, delete_btn;
-
-
+    EditText  price_input, count_input, sum_input;
+    Button save_btn, delete_btn, sum_btn;
+    AutoCompleteTextView name_input;///////////autocomplete text = name/////////////
+    //String[] names ={"ромашка","роза","гладиолус"};
+    String[] names = {DatabaseHelper.COLUMN_NAME};
+SimpleCursorAdapter adapter;
     DatabaseHelper sqlHelper;
     SQLiteDatabase db;
     Cursor userCursor;
@@ -28,12 +33,21 @@ public class EditSalesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_sales);
         name_input = findViewById(R.id.name);
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, names);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        /*не работает >=( // db = sqlHelper.getReadableDatabase();
+        userCursor = db.query(DatabaseHelper.TABLE_NOMENCLATURE,new String[]{"_id", "name"}, null, null, null, null, null);
+        //noinspection deprecation
+        adapter = new SimpleCursorAdapter(this, android.R.layout.simple_spinner_item,userCursor, new String[] {"name"}, new int[] {android.R.id.text1});
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);*/
+        name_input.setAdapter(adapter);
         price_input = findViewById(R.id.price);
         count_input = findViewById(R.id.count_sales);
         sum_input = findViewById(R.id.sum);
 
         save_btn = findViewById(R.id.saveButton);
         delete_btn = findViewById(R.id.deleteButton);
+        sum_btn = findViewById(R.id.sumButton);
 
         sqlHelper = new DatabaseHelper(this);
         db = sqlHelper.getWritableDatabase();
@@ -77,4 +91,13 @@ public class EditSalesActivity extends AppCompatActivity {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
         startActivity(intent);
     }
+
+    public void sumFunction(View view) {
+        int price = Integer.parseInt(String.valueOf(price_input.getText()));
+        int count = Integer.parseInt(String.valueOf(count_input.getText()));
+        int sum = price*count;
+        sum_input.setText(Integer.toString(sum));
+    }
+
+
 }
